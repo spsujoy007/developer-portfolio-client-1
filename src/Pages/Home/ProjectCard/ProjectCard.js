365457847f02b8db000f2132e2b119a88f4a353e
Sujoy Paul <freelancersujoy@gmail.com>
2023-09-23 20:07:06 +0600

    change ing project card and add pagination

--- src/Pages/Home/ProjectCard/ProjectCard.js
+++ src/Pages/Home/ProjectCard/ProjectCard.js
@@ -7,18 +7,20 @@ const ProjectCard = ({project, handleDetailpage}) => {
     // const detailProject = {image, title, tools}
 
     return (
-        <div className='max-w-[550px] relative overflow-hidden bg-white mainCard z-[100]'>
+        <div className='max-w-[550px] relative overflow-hidden bg-black mainCard z-[100]'>
 
             <div className='h-full w-full absolute z-10 cardOverFLowUp  flex items-center justify-center'>
                 <div className=''>
-                    <div className='w-[80%] mx-auto'>
-                        <h1 className={`text-3xl title text-center  text-white ${details.length < 1 && 'mb-10' }`}>{title}</h1>
+                    <div className='w-[80%] mx-auto '>
+                        <h1 className={`md:text-3xl text-2xl title text-center  text-white ${details.length < 1 && 'mb-10' }`}>{title}</h1>
                         <p className='text-center'>{details.slice(0,60)}</p>
                     </div>
 
                     <div className='flex items-center justify-center flex-wrap gap-5 mt-4'>
-                        <a target='_blank' rel="noreferrer" className='text-lg flex items-center gap-2 text-white hover:text-[#35C3E3] hover:border-[#35C3E3] duration-200 px-3 py-1 rounded-full border-[1px]' href={codelink}><FaCode></FaCode> Code link</a>
-                        <a target='_blank' rel="noreferrer" className='text-lg flex items-center gap-2 text-white px-3 py-1 rounded-full border-[1px] hover:text-[#35C3E3] hover:border-[#35C3E3] duration-200' href={demolink}><FaLaptop></FaLaptop> Live link</a>
+                        <div className='md:flex items-center justify-center flex-wrap gap-5 hidden'>
+                            <a target='_blank' rel="noreferrer" className='text-lg flex items-center gap-2 text-white hover:text-[#35C3E3] hover:border-[#35C3E3] duration-200 px-3 py-1 rounded-full border-[1px]' href={codelink}><FaCode></FaCode> Code link</a>
+                            <a target='_blank' rel="noreferrer" className='text-lg flex items-center gap-2 text-white px-3 py-1 rounded-full border-[1px] hover:text-[#35C3E3] hover:border-[#35C3E3] duration-200' href={demolink}><FaLaptop></FaLaptop> Live view</a>
+                        </div>
 
                         <Link to={`/project/${_id}`} className='text-lg detailBtn flex items-center px-3 gap-2 text-white  py-1 rounded-full border-[1px] hover:text-[#35C3E3] hover:border-[#35C3E3] duration-200' href={codelink}><FaInfoCircle></FaInfoCircle><span>Detail</span></Link>
                         
@@ -30,11 +32,12 @@ const ProjectCard = ({project, handleDetailpage}) => {
                 <img src={image} alt="" />
             </div>
 
-            <div className='h-full w-full absolute z-10 top-0 cardOverFLowDown flex items-center justify-center'>
+            <div className='h-full w-full absolute z-10 md:top-5 top-0 cardOverFLowDown flex items-center justify-center'>
                 <div>
-                    <h1 className='text-3xl title'>{title}</h1>
-                    <p className='text-center text-white'>This is an note pad type website</p>
-                    <p className='text-center text-white text-sm mt-5 md:block none'>Hover to see</p>
+                    <h1 className='md:text-3xl text-2xl title'>{title}</h1>
+                    <p className='text-center text-white'>Let's explore the website with good experience.</p>
+                    <p className='text-center text-white text-sm mt-5 md:block hidden'>Hover to see</p>
+                    <p className='text-center text-white text-sm mt-5 block md:hidden'>click to see</p>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Projects/Projects.js
+++ src/Pages/Home/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContainItem from '../../../Components/Container/ContainItem';
 import CommonHeading from '../../../Components/CommonHeading/CommonHeading';
 import ProjectCard from '../ProjectCard/ProjectCard';
@@ -11,9 +11,12 @@ import './Projects.css'
 const Projects = () => {
     useTitle('PROJECTS OF')
     const navigate = useNavigate()
+    const [firstPage, setFirstPage] = useState(0);
+    const [lastPage, setLastPage] = useState(3);
 
+    
     const Ptitle = useLocation()
-
+    
     const {data: projects = [], refetch, isLoading } = useQuery({
         queryKey: ['projects'],
         queryFn: async() => {
@@ -22,6 +25,14 @@ const Projects = () => {
             return data
         }
     })
+
+    const pages = [];
+    const numofpage = Math.ceil(projects.length / 4);
+    for(let i = 0; i < numofpage; i++ )
+    {
+        pages.push({num: i+1})
+    }
+
     refetch()
 
     if(isLoading){
@@ -33,37 +44,58 @@ const Projects = () => {
     return (
         <div >
             <ContainItem>
-            <div className={`px-5  bglineleft ${Ptitle.pathname === '/projects' && "py-16 md:max-w-[1240px] mx-auto"}`} id='projects'>
+            <div className={`px-5  bglineleft ${Ptitle.pathname === '/projects' && "py-16 md:max-w-[1240px] min-h-screen mx-auto"}`} id='projects'>
             <CommonHeading>Projects</CommonHeading>
             <p className='text-center mb-10 text-base-100'>An accomplished individual with a powerful background in <span className='text-sky-500'>
                 React, JavaScript, HTML and CSS, Express JS, Node
                 JS, Tailwind CSS, and MongoDB,</span> I feel within the power of the web and have an intense want to be told the way to improve the web' absolute core practicality and to even be concerned in its future development. He conjointly
                 incorporates a superb understanding of the terribly latest <span>Programming tricks.</span></p>
-            <div className='flex flex-wrap justify-center gap-6'>
+            <div>
+            <div className={`flex flex-wrap justify-center gap-6`}>
                 
-            {
-                    Ptitle.pathname === '/' ?
-                    <>
-                    {
-                    projects.slice(0, 3).map(project => <ProjectCard
-                        key={project.id}
-                        project={project}
-                    ></ProjectCard>)
-                    } 
-                    </>
-                    :
-                    <>
-                    {
-                    projects.map(project => <ProjectCard
-                        key={project.id}
-                        project={project}
-                    ></ProjectCard>)
-                }
-                    </>
-                }
-
-                
+                {
+                        Ptitle.pathname === '/' ?
+                        <>
+                        {
+                        projects.slice(0, 3).map(project => <ProjectCard
+                            key={project.id}
+                            project={project}
+                        ></ProjectCard>)
+                        } 
+                        </>
+                        :
+                        <>
+                        {
+                        projects.slice(firstPage, lastPage).map(project => <ProjectCard
+                            key={project.id}
+                            project={project}
+                        ></ProjectCard>)
+                    }
+                        </>
+                    }
+                </div>
             </div>
+
+            {/* pagination start ///////////////////////////////// */}
+            <div className={`${Ptitle.pathname === '/' ? "hidden": 'flex justify-center mt-5'}`}>
+            <div className='flex gap-1 '>
+                {
+                    pages.map(({num}) => <button
+                        onClick={() => {
+                            setFirstPage(num*3 - 3)
+                            setLastPage(3*num)
+                        }}
+                        key={num} 
+                        className={`${firstPage/3+1 === num ? 'bg-black border-white':'bg-sky-500'} rounded-md border-2 border-sky-600 text-white py-2 px-4`}
+                     >
+                        {num}
+                     </button>)
+                }
+            </div>
+            </div>
+            {/* pagination end //////////////////////////////////// */}
+
+
                 {
                     Ptitle.pathname  === '/' &&
                     <div className='flex justify-center w-full mt-5'>
